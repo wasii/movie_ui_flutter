8efe1ee4e1d35f177d9289f2d92c08090ca0152e
abdWasiq <abdulwdxba@hotmail.com>
2022-11-17 13:42:47 +0500

    added animations

--- movie_ui/lib/screens/home/components/movie_card.dart
+++ movie_ui/lib/screens/home/components/movie_card.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:movie_ui/screens/details/detail_screen.dart';
@@ -15,55 +16,56 @@ class MovieCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: InkWell(
-        onTap: (() => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => DetailScreen(movie: movie),
-          ),
-            )),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  boxShadow: [kDefaultShadow],
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(movie.poster)
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: kDefaultPadding / 2,
-              ),
-              child: Text(
-                movie.title,
-                style: Theme.of(context).textTheme.headline5?.copyWith(
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/icons/star_fill.svg",
-                  height: 20,
-                ),
-                SizedBox(width: kDefaultPadding / 2,),
-                Text(
-                  "${movie.rating}",
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
-              ],
-            )
-          ],
-        ),
+      child: OpenContainer(
+        openElevation: 0,
+        closedElevation: 0,
+        closedBuilder: (context, index) => buildMovieCard(context), 
+        openBuilder: (context, index) => DetailScreen(movie: movie),
       ),
     );
+  }
+
+  Column buildMovieCard(BuildContext context) {
+    return Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [kDefaultShadow],
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(movie.poster)
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: kDefaultPadding / 2,
+            ),
+            child: Text(
+              movie.title,
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                "assets/icons/star_fill.svg",
+                height: 20,
+              ),
+              SizedBox(width: kDefaultPadding / 2,),
+              Text(
+                "${movie.rating}",
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ],
+          )
+        ],
+      );
   }
 }
